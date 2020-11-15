1c0282c9fb91cf90a2319f5f69ee60abe64d77c9
BeforeLast <Bfr.Last@Gmail.com>
2020-11-15 18:55:14 +0700

    Removed unused libraries, Added comments

--- src/back-end/app.py
+++ src/back-end/app.py
@@ -1,52 +1,44 @@
-import imghdr
 import os
 from flask import Flask, render_template, request, send_from_directory, jsonify, redirect
 from werkzeug.utils import secure_filename
 from csim import *
 
 app = Flask(__name__)
-app.config['Extension'] = ('.pdf', '.txt', '.html')
-app.config['ExtensionS'] = ('*.pdf', '*.txt', '*.html')
-app.config['Path'] = '../../test/'
-app.config['Query'] = 'query'
+app.config['Extension'] = ('.pdf', '.txt', '.html') # Acceptable extension for uploaded file
+app.config['ExtensionS'] = ('*.pdf', '*.txt', '*.html') # File extension for returning search
+app.config['Path'] = '../../test/' # Path to documents folder (relative to current app.py)
+app.config['Query'] = 'query' # Parameter from reactjs
 
-# @app.route('/')
-# def index():
-#     dir = os.listdir(app.config['Path'])
-#     return render_template('index.html',files=dir)
-
-@app.route('/upload', methods=['POST','GET'])
+@app.route('/upload', methods=['POST','GET']) # Route to upload file
 def upload_files():
     if request.method == "POST":
         upDoc = request.files["file"]
-        upDocName = secure_filename(upDoc.filename)
+        upDocName = secure_filename(upDoc.filename) # Secure a file name
         print(upDoc)
-        if upDocName != '':
-            if not upDocName.lower().endswith(app.config['Extension']):
+        if upDocName != '': # Filename is legal
+            if not upDocName.lower().endswith(app.config['Extension']): # Checking file extension
                 return 'File extension not allowed'
-            upDoc.save(os.path.join(app.config['Path'], upDocName))
-        else : 
+            upDoc.save(os.path.join(app.config['Path'], upDocName)) # Upload to documents folder
+        else : # Illegal file name
             return 'File name error!'
-        return 'File Uploaded!'
+        return 'File Uploaded!' 
 
-@app.route('/database/<filename>',methods=['GET']) # return file
+@app.route('/database/<filename>',methods=['GET']) # Route to return file
 def view_file(filename):
-    if isfilenotempty(app.config['Path'],filename):
-        return send_from_directory(app.config['Path'], filename)
-    return redirect('/error/emptyfile')
+    if isfilenotempty(app.config['Path'],filename): # Check if file is not empty
+        return send_from_directory(app.config['Path'], filename) # File is not empty and returned to be shown
+    return redirect('/error/emptyfile') # File is empty and redirect to route /error/emptyfile
 
-@app.route('/error/emptyfile',methods=['GET'])
-def errorq():
-    return 'ERROR : FILE IS EMPTY'
+@app.route('/error/emptyfile',methods=['GET']) # Route to redirect error : empty file
+def errorq(): 
+    return 'ERROR : FILE IS EMPTY' # Return a error message
 
-@app.route('/search/',methods=['POST','GET'])
+@app.route('/search/',methods=['POST','GET']) # Route to search query
 def search():
-    query = request.get_json()
-    # print(searchq(query[app.config['Query']],app.config['Path'],app.config['ExtensionS']))
-    return jsonify(searchq(query[app.config['Query']],app.config['Path'],app.config['ExtensionS']))
+    query = request.get_json() # Get query from reactjs
+    return jsonify(searchq(query[app.config['Query']],app.config['Path'],app.config['ExtensionS'])) # Return array of dict
 
-@app.route('/table',methods=['POST','GET'])
+@app.route('/table',methods=['POST','GET']) # Route to search result table
 def table():
-    query = request.get_json()
-    # print(searchqt(query[app.config['Query']],app.config['Path'],app.config['ExtensionS']))
-    return jsonify(searchqt(query[app.config['Query']],app.config['Path'],app.config['ExtensionS']))
+    query = request.get_json() # Get query from reactjs
+    return jsonify(searchqt(query[app.config['Query']],app.config['Path'],app.config['ExtensionS'])) # Return table in form of array [[..],[..],..,[..]]
--- src/back-end/csim.py
+++ src/back-end/csim.py
@@ -3,8 +3,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 
-#import numpy as np
-
 # Word Count
 import collections
 
@@ -12,8 +10,8 @@ import collections
 import math
 
 # Reading doc
-from bs4 import BeautifulSoup # html
-from pdfminer.high_level import extract_text # pdf
+from bs4 import BeautifulSoup # read html
+from pdfminer.high_level import extract_text # read pdf
 
 # Reading first sentence
 import re
@@ -24,12 +22,16 @@ from os.path import join
 from os import listdir
 from os import path
 
-def clear(x): # terima string
+def clear(x): # Clear strings from symbols
+    # x         : string (filled with symbols)
+    # output    : srings (no symbols)
     if x != None:
         return x.lower().replace('–',' ').replace('-',' ').replace('(','').replace(')',' ').replace(',','').replace('.','').replace('!','').replace('?','').replace('"',' ')
     return None
 
-def tokenit(x): # ubah string ke list
+def tokenit(x): # Tokenizing document
+    # x         : string
+    # output    : array of strings
     ban = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 
     'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 
     'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 
@@ -40,22 +42,31 @@ def tokenit(x): # ubah string ke list
     tokenswsw = [PorterStemmer().stem(i) for i in tokens if not i in ban]
     return tokenswsw
 
-def wordcount(x): # terima list, ubah ke count
+def wordcount(x): # Count occurence of words
+    # x         : array of strings
+    # output    : array of tuple (A,B), A is word, B is occurence of the word
     count = collections.Counter(x)
     wcount = (list(count.items()))
     return wcount
 
-def totalword(x): # terima string, return total word
+def totalword(x): # Sum of all words
+    # x         : array of dict
+    # output    : integer
     doc = wordcount(list(clear(x).split()))
     sum = 0
     for i in range(len(doc)):
         sum += doc[i][1]
     return sum
 
-def isfilenotempty(dir,x): # dir : directory folder, x : namafile
+def isfilenotempty(dir,x): # Check file is not empty
+    # dir       : strings (folder directory)
+    # x         : strings (file name)
+    # output    : boolean (true if file is not empty, otherwise false)
     return (path.getsize(join(dir,x)) > 0) and (path.isfile(join(dir,x)))
 
-def opendoc(x): # terima directory
+def opendoc(x): # Open a document and convert it into strings
+    # x         : strings (file directory)
+    # output    : strings
     if x.lower().endswith('.html'):
         return BeautifulSoup(open(x),"html.parser").get_text()
     elif x.lower().endswith('.pdf'):
@@ -63,31 +74,38 @@ def opendoc(x): # terima directory
     else :
         return open(x,"r",encoding='utf8').read()
 
-def readtitle(x): # baca judul (asumsi di line pertama) dari dokumen
+def readtitle(x): # Read document title
+    # x         : strings (file directory)
+    # output    : strings
     A = opendoc(x).splitlines()
     i = 0
-    if (len(A) > 0) :
+    if (len(A) > 0) : # Searching title
         while (i < len(A)) and (A[i].replace(' ','') == ''):
             i += 1
         if (i < len(A)) and (A[i] != ''):
-            return (A[i])
-    return 'Error : title not found.'
+            return (A[i]) # Title is found and return the title in strings
+    return 'Error : title not found.' # Title not found and return an error
 
-def readfirstsen(x): # baca judul (asumsi di line pertama) dari dokumen
+def readfirstsen(x): # Read first sentence in document's paragraph
+    # x         : strings (file directory)
+    # output    : strings
     if (readtitle(x) != 'Error : title not found.'):
         A = opendoc(x).splitlines()
         i = 0
         tfound,found = False,False
-        while (i < len(A)):
-            if (tfound) and A[i].replace(' ','') != '':
+        while (i < len(A)): # Seaching first sentence in document.
+            if (tfound) and A[i].replace(' ','') != '': # Read past title (and title already located)
                 B = re.split("\.\s+",A[i])
                 return (B[0])
-            if (A[i] == readtitle(x)): # cari line yang ga kosong
+            if (A[i] == readtitle(x)): # Found title
                 tfound = True
             i += 1
-    return 'Error : body not found.'
+    return 'Error : body not found.' # First sentence is not found, returning error string
 
-def sim(Q,D): # return similiarity
+def sim(Q,D): # Cosine similiarity
+    # Q         : strings (query)
+    # D         : strings (document in strings)
+    # output    : float (0-100)
     A = wordcount(tokenit(Q))
     B = wordcount(tokenit(D))
     dot,lQ,lD = 0,0,0
@@ -102,20 +120,27 @@ def sim(Q,D): # return similiarity
     else:
         return 0
 
-def searchq(Q,dir,ext): # Q = Query(string), dir = directory(string)
-    # {'title':'','totalword':0,'similiarity':0,'firstsentence':'','docdir':''}
-    A = [] # array of file directory
+def searchq(Q,dir,ext): # Search similiarity for all existing documents in directory
+    # Q         : strings (query)
+    # dir       : strings (documents file directory)
+    # ext       : tuple of strings (file extension allowed)
+    # output    : array of dict (below is the format of the dict)
+    #             {'title':'','totalword':0,'similiarity':0,'firstsentence':'','filename':''}
+    A = [] # Array of file directory
     for types in ext:
         A.extend(glob(join(dir, types)))
-    S = [] # array of search result
+    S = [] # Array of search result
     for i in range(len(A)):
         S.append({"title":readtitle(A[i]),"totalword":totalword(opendoc(A[i])),"similiarity":sim(Q,opendoc(A[i])),"firstsentence":readfirstsen(A[i]),"filename":(A[i].partition('\\'))[2]})
     if len(S) != 0:
         return S
     else :
-        return 0
+        return 0 # No result found (either no document or no file with required exstension)
 
-def simtable(Q,D): # return similiarity
+def simtable(Q,D): # Creating vector for document from query
+    # Q         : strings (query)
+    # D         : strings (document in strings)
+    # output    : array of int (vector)
     A = wordcount(tokenit(Q))
     B = wordcount(tokenit(D))
     C = [0 for i in range(len(A))]
@@ -125,16 +150,20 @@ def simtable(Q,D): # return similiarity
                 C[i] += B[j][1]
     return C
 
-def searchqt(Q,dir,ext):
-    # Membuat base
-    A = [] # array of namafile
+def searchqt(Q,dir,ext): # Creating vector table to be compared with query for all documents with correct extension
+    # Q         : strings (query)
+    # dir       : strings (document file directory)
+    # ext       : tuple of strings (file extension allowed)
+    # output    : array of array of string and int
+    # Table Initialization
+    A = [] # Array of file directory
     for types in ext:
         A.extend(glob(join(dir, types)))
     AT = A.copy()
     for i in range(len(A)):
         A[i] = A[i].partition('\\')[2]
     A = ["Keywords","Query"] + A
-    # Membuat tabel
+    # Array (Table) processing
     B = wordcount(tokenit(Q))
     R = [[B[i][0],B[i][1]] for i in range(len(B))]
     for i in range(0,len(AT)):
@@ -143,13 +172,3 @@ def searchqt(Q,dir,ext):
             (R[j]).append(N[j])
     R = [A] + R
     return R
-     
-# print(searchq("RSA Prime",'../test/',('*.pdf', '*.txt', '*.html')))
-# print(opendoc("../doc/H.pdf"))
-# print(opendoc("../doc/b.html"))
-# print(opendoc("../doc/a.txt"))
-# print(isfilenotempty('./test/','a.txt'))
-# print(readtitle('./test/haci.pdf'))
-#print(sim("RSA rsa prime",opendoc("../doc/a.txt")))
-#wordlist = [avquery[i][0] for i in range(len(avquery))]
-#countword = [avquery[i][1] for i in range(len(avquery))]
